3d01bfabe2f6901b283ba37ff0b481ff3b0b3c9b
sixshans <shandy.pbw@gmail.com>
2023-11-21 00:05:38 +0700

    update

--- src/test/java/StepDef/LaptopDetailStepDef.java
+++ src/test/java/StepDef/LaptopDetailStepDef.java
@@ -1,15 +1,18 @@
 package StepDef;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.After;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -21,14 +24,15 @@ public class LaptopDetailStepDef {
 
     @Before
     public void setup(){
-        driver = new FirefoxDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
     @Given("User on Tokopedia website home page")
     public void user_on_tokopedia_website_home_page() {
         driver.get("https://www.tokopedia.com/");
-        //Assert Homepage
+        // Assert Homepage
         WebElement searchBarLocator = driver.findElement(By.xpath("//input[@type='search']"));
         Assert.assertTrue(searchBarLocator.isDisplayed());
     }
@@ -43,7 +47,7 @@ public class LaptopDetailStepDef {
 
     @And("User directed into list of laptop MSI")
     public void user_directed_into_list_of_laptop_msi() {
-        //Wait for "Produk" tab to be visible
+        // Wait for "Produk" tab to be visible
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement ProductPage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Produk']")));
         Assert.assertTrue(ProductPage.isDisplayed());
@@ -52,25 +56,23 @@ public class LaptopDetailStepDef {
 
     @When("User click product")
     public void user_click_product() {
-        //Scroll down 500 pixels to see product
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,500)");
-        //Click the product
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        WebElement ProductSection = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='css-jza1fo'])[6]")));
-//        ProductSection.click();
-        driver.findElement(By.xpath("//div[@class='css-jza1fo'])[6]"));
+        // Click the product
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement ProductSection = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div/div[2]/div/div[2]/div[4]/div[1]/div[6]")));
+//        WebElement ProductSection = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@data-testid='divSRPContentProducts'])[1][6]")));
+        ProductSection.click();
     }
 
     @Then("User directed to product detail page")
     public void user_directed_to_product_detail_page() {
-        //Wait before "Detail" to be visible and verify
+        // Wait before "Detail" to be visible and verify
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement ProductDetail = wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//p[text()='Detail']"))));
         Assert.assertTrue(ProductDetail.isDisplayed());
 
-        //Wait before Product Label to be visible and verify
-        WebElement ProductLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@data-testid='lblPDPDetailProductName'")));
+        // Wait before Product Label to be visible and verify
+//        WebElement ProductLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@data-testid='lblPDPDetailProductName'")));
+        WebElement ProductLabel = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[4]/div[1]/h1[1]")));
         Assert.assertTrue((ProductLabel.isDisplayed()));
 
     }
